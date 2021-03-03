2d81a7cb5993bb14db26afc334398fa7ac73a894
Michael Joyce <mjoyce@sfu.ca>
2021-03-03 13:52:57 -0800

    config and a working app

---
+++ src/Menu/Builder.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) 2021 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+/**
+ * Class to build some menus for navigation.
+ */
+class Builder implements ContainerAwareInterface {
+    use ContainerAwareTrait;
+
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authChecker;
+
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    public function __construct(FactoryInterface $factory, AuthorizationCheckerInterface $authChecker, TokenStorageInterface $tokenStorage) {
+        $this->factory = $factory;
+        $this->authChecker = $authChecker;
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    private function hasRole($role) {
+        if ( ! $this->tokenStorage->getToken()) {
+            return false;
+        }
+
+        return $this->authChecker->isGranted($role);
+    }
+
+    /**
+     * Build a menu for navigation.
+     *
+     * @return ItemInterface
+     */
+    public function mainMenu(array $options) {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttributes([
+            'class' => 'nav navbar-nav',
+        ]);
+
+        $browse = $menu->addChild('Browse', [
+            'uri' => '#',
+            'label' => 'Browse',
+        ]);
+        $browse->setAttribute('dropdown', true);
+        $browse->setLinkAttribute('class', 'dropdown-toggle');
+        $browse->setLinkAttribute('data-toggle', 'dropdown');
+        $browse->setChildrenAttribute('class', 'dropdown-menu');
+
+//        $browse->addChild('Projects', ['route' => 'project_index']);
+
+        if ($this->hasRole('ROLE_CONTENT_ADMIN')) {
+            $divider = $browse->addChild('divider_content', [
+                'label' => '',
+            ]);
+            $divider->setAttributes([
+                'role' => 'separator',
+                'class' => 'divider',
+            ]);
+//            $browse->addChild('Time Cards', ['route' => 'time_card_index']);
+        }
+
+        if ($this->hasRole('ROLE_ADMIN')) {
+            $divider = $browse->addChild('divider_admin', [
+                'label' => '',
+            ]);
+            $divider->setAttributes([
+                'role' => 'separator',
+                'class' => 'divider',
+            ]);
+            //            $browse->addChild('Formats', ['route' => 'format_index']);
+        }
+
+        return $menu;
+    }
+
+    /**
+     * Build a menu for navigation.
+     *
+     * @return ItemInterface
+     */
+    public function footerMenu(array $options) {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttributes([
+            'class' => 'nav navbar-nav',
+        ]);
+
+        $menu->addChild('Home', [
+            'route' => 'homepage',
+        ]);
+        $menu->addChild('Privacy', [
+            'route' => 'privacy',
+        ]);
+        $menu->addChild('DHIL @ GitHub', [
+            'uri' => 'https://github.com/sfu-dhil',
+        ]);
+
+        return $menu;
+    }
+}
