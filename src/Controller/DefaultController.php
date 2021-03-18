16e10ea1896209a316a3626e16568111c607fd60
Michael Joyce <mjoyce@sfu.ca>
2021-03-18 10:45:28 -0700

    use the query builder.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,7 +13,9 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Bundle\PaginatorBundle\Definition\PaginatorAwareInterface;
 use Knp\Component\Pager\PaginatorInterface;
-use Nines\SolrBundle\Client\Builder;
+use Nines\SolrBundle\Client\ClientBuilder;
+use Nines\SolrBundle\Logging\SolrLogger;
+use Nines\SolrBundle\Query\QueryBuilder;
 use Nines\SolrBundle\Query\Result;
 use Nines\UtilBundle\Controller\PaginatorTrait;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -45,41 +47,29 @@ class DefaultController extends AbstractController implements PaginatorAwareInte
      * @Route("/solr", name="solr")
      * @Template
      */
-    public function solrAction(Request $request, Builder $builder, EntityManagerInterface $em, PaginatorInterface $paginator) {
+    public function solrAction(Request $request, SolrLogger $logger, QueryBuilder $qb, EntityManagerInterface $em, PaginatorInterface $paginator) {
         $q = $request->query->get('q');
         $filters = $request->query->get('filter', []);
         $page = (int) $request->query->get('page', 1);
-        $pageSize = 10; //(int)$this->getParameter('page_size');
+        $pageSize = (int)$this->getParameter('page_size');
         $qr = null;
         $paginated = null;
         if ($q) {
-            $client = $builder->build();
-            $query = $client->createSelect();
-
-            $query->setStart(($page - 1) * $pageSize);
-            $query->setRows($pageSize);
-
-            $query->setQuery($q);
-            $query->setQueryDefaultField('content_txt');
+            $qb->setQueryString($q);
+            $qb->setDefaultField('content_txt');
 
             foreach ($filters as $key => $values) {
-                $terms = join(' or ', $values);
-                $query->createFilterQuery('fq_' . $key)->addTag('exclude')->setQuery("{$key}:({$terms})");
+                $qb->addFilter($key, $values);
             }
+            $qb->setHighlightFields('content_txt');
+            $qb->addFacetField('type', 'type_s');
+            $qb->addFacetField('genre', 'genres_ss');
+            $query = $qb->getQuery();
 
-            $hl = $query->getHighlighting();
-            $hl->setFields('content_txt');
-            $hl->setSimplePrefix("<span class='hl'>");
-            $hl->setSimplePostfix('</span>');
-
-            $fs = $query->getFacetSet();
-            $fs->createFacetField('type')->setField('type_s')->getLocalParameters()->addExcludes(['exclude']);
-            $fs->createFacetField('genre')->setField('genres_ss')->getLocalParameters()->addExcludes(['exclude']);
-
-            dump($query);
-
-            $paginated = $paginator->paginate([$client, $query], $page, $pageSize);
+            $logger->startQuery($query);
+            $paginated = $paginator->paginate([$qb->getClient(), $query], $page, $pageSize);
             $qr = new Result($paginated->getCustomParameter('result'), $em, $paginator);
+            $logger->stopQuery();
         }
 
         return [
